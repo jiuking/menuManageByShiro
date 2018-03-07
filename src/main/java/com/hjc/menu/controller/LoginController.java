edb06b22df703711e11748cf71a6bab236c17997
hjc <lingshao2010@126.com>
2018-03-07 22:34:37 +0800

    修改shiro拦截状态

--- src/main/java/com/hjc/menu/controller/LoginController.java
+++ src/main/java/com/hjc/menu/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.hjc.menu.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+
+    private final static Logger log = LoggerFactory.getLogger(LoginController.class);
 
     //返回登录页面
     @RequestMapping("/index")
@@ -19,13 +23,15 @@ public class LoginController {
 
     @RequestMapping(value = "login",produces = {"application/json;charset=UTF-8"})
 //    @ResponseBody
-    public String login() {
+    public String login(String username,String password) {
         String result = "";
+        log.info("用户名:{}",username);
+        log.info("密码:{}",password);
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("admin","123456");
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
         subject.login(token);
 //        result = "SUCCESS";//登录成功
-        return "login";
+        return "redirect:/success";
     }
 
     @RequestMapping(value = "/success", produces = {"application/json;charset=UTF-8"})
