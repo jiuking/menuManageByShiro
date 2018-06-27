1ca8d855754ffa24c40ce24b78583cb94f4d6ec5
hjc <lingshao2010@126.com>
2018-06-28 00:29:44 +0800

    annotation中 方法指定 default说明

--- src/test/FieldValidate.java
+++ src/test/FieldValidate.java
@@ -6,6 +6,8 @@ import java.lang.annotation.*;
 public @interface FieldValidate {
     enum Status {NotEmpty, NotNull, NoNegativeDecimal, Null}
 
+    //添加default 默认的值，在添加该自定义注解时，不需要强制指定一种类型。如果不指定类型报错：'status' missing though required
+//    Error:(27, 13) java: 对于元素 'status', 注释 @FieldValidate 缺少默认值
     Status status() default Status.Null;
 
     String message() default "";
